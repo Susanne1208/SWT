526e7cbee9ff45ffbb4b01c57e77a0e54c58e402
Emma Rolsted <emma-k-r@hotmail.com>
2018-05-13 22:20:25 +0200

    Added ProximityDetection class and unittest

---
+++ ATM/ATM.Test.Unit/ProximityDetectionTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATM.Data;
+using ATM.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ATM.Test.Unit
+{
+    [TestFixture]
+    public class ProximityDetectionTest
+    {
+        private IProximityDetection _uut;
+        private TrackData _track1;
+        private TrackData _track2;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _uut = new ProximityDetection();
+            _track1 = Substitute.For<TrackData>();
+            _track2 = Substitute.For<TrackData>();
+        }
+
+
+        //Horizontal seperation less than 5000 meters
+
+
+        //vertical seperation less than 300 meters
+
+        //x1, y1, x2, y2,  alt1, alt2, result
+        [TestCase(11111,22222,33333,4444,8000,8100, true)] //Horizontal conflict
+        [TestCase(11111,22222,1122,2233,8000,8700, true)] // Vertical conflict
+        public void IsTracksInConflict_returnsTrue(int x1, int y1, int x2, int y2, int alt1, int alt2, bool result)
+        {
+            //track1 and track 2 is conflicting because of horizontal and vertical distance
+            _track1.X = x1;
+            _track1.Y = y1;
+            _track1.Altitude = alt1;
+
+            _track2.X = x2;
+            _track2.Y = y2;
+            _track2.Altitude = alt2;
+
+            Assert.AreEqual(result, _uut.IsTracksInConflict(_track1,_track2));
+
+
+        }
+
+        //x1, y1, x2, y2,  alt1, alt2, result
+        [TestCase(11111, 22222, 33333, 44444, 8000, 6000, false)] //Horizontal
+        [TestCase(11111, 22222, 40000, 50000, 8000, 7000, false)] // Vertical
+        [Test]
+        public void IsTracksInConflict_returnsFalse(int x1, int y1, int x2, int y2, int alt1, int alt2, bool result)
+        {
+            //track1 and track 2 is NOT conflicting because of distance
+            _track1.X = x1;
+            _track1.Y = y1;
+            _track1.Altitude = alt1;
+
+            _track2.X = x2;
+            _track2.Y = y2;
+            _track2.Altitude = alt2;
+
+            Assert.AreEqual(result, _uut.IsTracksInConflict(_track1, _track2));
+        }
+    }
+}
---
+++ ATM/ATM/ProximityDetection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using ATM.Data;
+using ATM.Interfaces;
+
+namespace ATM
+{
+    public class ProximityDetection : IProximityDetection
+    {
+
+        //Horizontal seperation less than 5000 meters
+        int HorizontalSeperation = 5000;
+
+        //vertical seperation less than 300 meters
+        int VerticalSeperation = 300;
+
+        public void CheckForSepeation(TrackData track1, TrackData track2)
+        {
+            //Formula: distance = sqrt((x1-x2)^2+(y1-y2)^2)
+            var horizantalDistance = Math.Sqrt(Math.Pow(track1.X - track2.X, 2) + Math.Pow(track2.Y - track2.Y, 2));
+            
+            //vertical seperation less than 300 meters
+            var veritalDistance = Math.Abs(track1.Altitude - track2.Altitude);
+
+            // If the to two planes tag is different and is conflicting with the minimum seperation.
+            if (track1.Tag != track2.Tag && veritalDistance < VerticalSeperation && horizantalDistance < HorizontalSeperation)
+            {
+                //A event needs to happen when the above statements is true...
+                //EventRendition ConflictEvent= new EventRendition(track1.Tag, track2.Tag, DateTime.Now);
+            }
+        }
+
+        public bool IsTracksInConflict(TrackData track1, TrackData track2)
+        {
+            //Formula: distance = sqrt((x1-x2)^2+(y1-y2)^2)
+            var horizantalDistance = Math.Sqrt(Math.Pow(track1.X - track2.X, 2) + Math.Pow(track2.Y - track2.Y, 2));
+
+            //vertical seperation less than 300 meters
+            var veritalDistance = Math.Abs(track1.Altitude - track2.Altitude);
+
+            if (track1.Tag != track2.Tag && veritalDistance < VerticalSeperation &&
+                horizantalDistance < HorizontalSeperation)
+            {
+                return true;
+            }
+            else return false;
+
+            //return horizantalDistance <= HorizontalSeperation && veritalDistance <= VerticalSeperation;
+        }
+    }
+}
