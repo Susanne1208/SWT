a3d5f64e4211720d8f5921f2cf94a46a52960b09
Susanne Olsen <susanne_olsen@live.dk>
2018-05-13 18:06:59 +0200

    Added  EventRendition class plus interface. Added classes for testing Event- and TrackRendition.

---
+++ ATM/ATM/EventRendition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public class EventRendition : IEventRendition
+    {
+        public EventRendition()
+        {
+
+        }
+
+        public void LogToFile()
+        {
+
+        }
+
+        public void PrintEvent()
+        {
+
+        }
+    }
+}
