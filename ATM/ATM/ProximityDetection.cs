22c23d5c62dca672e57de4cc289bac43d04f1aac
Emma Rolsted <emma-k-r@hotmail.com>
2018-05-14 23:46:27 +0200

    Changed proximityDetection and added a class for logData

--- ATM/ATM/ProximityDetection.cs
+++ ATM/ATM/ProximityDetection.cs
@@ -15,13 +15,16 @@ namespace ATM
         private int VerticalSeperation = 300; //vertical seperation less than 300 meters
 
         //private readonly IEventRendition _eventRendition;
-        private IEventRendition _eventRendition;
+        private readonly IEventRendition _eventRendition;
+        private readonly ProximityDetectionData _proximityDetectionData;
 
 
-        public ProximityDetection(IEventRendition eventRendition)
+
+        public ProximityDetection(IEventRendition eventRendition, ProximityDetectionData proximityDetectionData)
         {
             //Need to call LogToFile
             _eventRendition = eventRendition;
+            _proximityDetectionData = proximityDetectionData;
         }
 
         public void CheckProximityDetection(List<ITrackData> trackDataList)
@@ -38,8 +41,8 @@ namespace ATM
                     double y2 = track2.Y;
                     double alt1 = track1.Altitude;
                     double alt2 = track1.Altitude;
-
                     
+
                     //Formula: sqrt((x1-x2)^2+(y1-y2)^2)
                     var horizantalDistance = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
 
@@ -49,9 +52,16 @@ namespace ATM
                     // If the to two planes tag is different and is conflicting with the minimum seperation.
                     if (track1.Tag != track2.Tag && veritalDistance < VerticalSeperation && horizantalDistance < HorizontalSeperation)
                     {
-                        //Kan ikke kalde funktioner.... 
-                        _eventRendition.LogToFile();
+                        //_proximityDetectionData(track1.Tag, track2.Tag, DateTime.Now);
+                        _proximityDetectionData.Tag1 = track1.Tag;
+                        _proximityDetectionData.Tag2 = track2.Tag;
+                        _proximityDetectionData.Timestamp = DateTime.Now;
+                        
+                        //Sending data to EventRendition class to be log and printed on console application
+                        //_eventRendition.LogToFile(_proximityDetectionData);
+                        //_eventRendition.PrintEvent(_proximityDetectionData);
                         _eventRendition.PrintEvent();
+                        _eventRendition.LogToFile();
 
                         //the time og the event needs to be logged to files or application
                         //track1.TimeStamp = DateTime.Now;
