8b222acc1ae7057b43db385c358203c7a25237fe
Emma Rolsted <emma-k-r@hotmail.com>
2018-05-15 00:38:05 +0200

    Added ProximityDetection to EventRendition

--- ATM/ATM/EventRendition.cs
+++ ATM/ATM/EventRendition.cs
@@ -5,26 +5,52 @@
 ////using System.Text;
 ////using System.Threading.Tasks;
 
+using System.IO;
+
 namespace ATM
 {
     public class EventRendition : IEventRendition
     {
+        //private readonly ProximityDetectionData _proximityDetectionData;
+
+        //public EventRendition(ProximityDetectionData proximityDetectionData)
+        //{
+        //    _proximityDetectionData = proximityDetectionData;
+        //}
+
         public EventRendition()
         {
         }
 
         public void LogToFile()
         {
-            //string path = @"C:\Temp\Logfile.txt";
-            //string text = "Planes in conflict: " + seperationData.TAG1 + " and " + seperationData.TAG2 +
-            //                  "\nTime of occurance: " + seperationData.TimeStamp;
-            //File.WriteAllText(path, text);
-            //System.Console.WriteLine("TEST);
+
+        //    //string path = @"C:\Temp\Logfile.txt";
+        //    //string text = "Planes in conflict: " + seperationData.TAG1 + " and " + seperationData.TAG2 +
+        //    //                  "\nTime of occurance: " + seperationData.TimeStamp;
+        //    //File.WriteAllText(path, text);
+        //    //System.Console.WriteLine("TEST);
         }
 
+        //public void LogToFile()
+        //{
+        //    string path = @"C:\Temp\Logfile.txt";
+        //    string text = "Planes in conflict: " + _proximityDetectionData.Tag1 + " and " + _proximityDetectionData.Tag2 +
+        //                  "\nTime of occurance: " + _proximityDetectionData.Timestamp;
+
+        //    File.WriteAllText(path, text);
+        //}
+
         public void PrintEvent()
-        {
+        {   
+            //Print the collision waring to the console application
             //System.Console.WriteLine(seperationData);
         }
+
+        //public void PrintEvent()
+        //{
+        //    //Print the collision waring to the console application
+        //    System.Console.WriteLine(_proximityDetectionData);
+        //}
     }
 }
