52368928fec52cde3d83e5aa1ea2821b15692eb9
Julie Alberte <julie.steenshard@gmail.com>
2018-05-14 21:52:20 +0200

    Integrationtest started

--- ATM/ATM.Test.Integration/IT2_.cs
+++ ATM/ATM.Test.Integration/IT2_.cs
@@ -1,17 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using NUnit.Framework;
-using NSubstitute;
-
 
 namespace ATM.Test.Integration
 {
     [TestFixture]
-    public class IT1_
+    class IT2_
     {
         [SetUp]
         public void SetUp()
         {
 
         }
+
     }
 }
