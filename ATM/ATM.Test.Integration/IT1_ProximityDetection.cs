ac854b84eda082e9638d5b97228bcf2750e533c0
Susanne Olsen <susanne_olsen@live.dk>
2018-05-15 17:49:41 +0200

    Edited in integrationtest IT3. Deleted injection in EventRendition

--- ATM/ATM.Test.Integration/IT1_ProximityDetection.cs
+++ ATM/ATM.Test.Integration/IT1_ProximityDetection.cs
@@ -6,7 +6,7 @@ using NSubstitute;
 namespace ATM.Test.Integration
 {
     [TestFixture]
-    public class IT1_
+    public class IT1_ProximityDetection
     {
         [SetUp]
         public void SetUp()
